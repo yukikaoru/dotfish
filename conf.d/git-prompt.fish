48eb45e890fdc10a42d7de5a4dae328341fe6e21
YUKI Kaoru <yuki@yagni.jp>
2020-11-29 17:09:41 +0900

    change git status emoji

--- conf.d/git-prompt.fish
+++ conf.d/git-prompt.fish
@@ -16,10 +16,10 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '☀'
-set __fish_git_prompt_char_stagedstate '⭐'
-set __fish_git_prompt_char_untrackedfiles '☔'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '➕'
-set __fish_git_prompt_char_upstream_behind '➖'
+set __fish_git_prompt_char_dirtystate '🍺'
+set __fish_git_prompt_char_stagedstate '🍷'
+set __fish_git_prompt_char_untrackedfiles '🍶'
+set __fish_git_prompt_char_stashstate '🍹'
+set __fish_git_prompt_char_upstream_ahead '🍵'
+set __fish_git_prompt_char_upstream_behind '☕'
 
